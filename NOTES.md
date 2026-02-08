514c9dd3e257f1bd4d8c1413aa3c54ed058bac0a
vishnu <hikingfaab@gmail.com>
2026-02-08 16:51:37 +0000

    inital commit & bin folder

---
+++ NOTES.md
@@ -0,0 +1,3 @@
+All the file will go inside bin folder
+
+To run any file, say variables.rs, run cargo run -q --bin variables
---
+++ src/bin/variables.rs
@@ -0,0 +1,6 @@
+fn main() {
+    let age: i32 = 29;
+    let name: &str = "Vishnu";
+
+    println!("Name is {name} and age is {age}");
+}
