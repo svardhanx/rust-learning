d79bf44dc8cd26b08a3f0a20295ac192d91296cc
vishnu <hikingfaab@gmail.com>
2026-05-03 12:45:00 +0000

    feat: add two more ways for iterating over args in args.rs

--- src/bin/args.rs
+++ src/bin/args.rs
@@ -8,7 +8,22 @@ fn main() {
         std::process::exit(1);
     }
 
-    let user_choice: &String = &args[1];
+    // let user_choice: &String = &args[0];
 
-    println!("You entered {user_choice}")
+    // println!("You entered {user_choice}")
+
+    // for arg in &args {
+    //     println!("arg is {arg}")
+    // }
+
+    for idx in 1..args.len() {
+        println!("Index {} and value is {}", idx, args[idx])
+    }
+
+    println!("--------------------------------------------");
+
+    // OR
+    for (i, v) in args.iter().enumerate().skip(1) {
+        println!("Index {i} and value is {v}")
+    }
 }
