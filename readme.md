4d78d1bd2a560671eaff5ab9666866056b279b45
vishnu <vishnu.vardhan@aptagrim.com>
2026-07-17 16:08:32 +0530

    update readme with examples and refactor sum_array code for clarity

--- readme.md
+++ readme.md
@@ -1,1 +1,4 @@
 cargo run -q --bin <binary_name>
+
+tuple example in math_operations
+array declaration example in sum_array
--- src/bin/sum_array.rs
+++ src/bin/sum_array.rs
@@ -1,19 +1,17 @@
+fn main() {
+    let arr: [u32; 5] = [1, 2, 3, 4, 5];
 
-fn main(){
-    let arr: [u32; 5]= [1,2,3,4,5];
+    let result: u32 = sum_array(&arr);
 
-    let result : u32 = sum_array(&arr);
-
-    println!("Sum is {}", result)    
+    println!("Sum is {}", result)
 }
 
-fn sum_array(arr:&[u32; 5]) -> u32{
-
+fn sum_array(arr: &[u32; 5]) -> u32 {
     let mut res: u32 = 1;
 
     for a in arr {
         res *= a
     }
 
-    return res as u32
+    return res as u32;
 }
