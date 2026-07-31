e7ebdb5d3f1fac2ece5a6ea8b7b699094740ec69
vishnu <hikingfaab@gmail.com>
2026-07-31 06:22:01 +0000

    add strcuts file

--- src/bin/struct.rs
+++ src/bin/struct.rs
@@ -1,7 +1,40 @@
+use std::println;
+
 #[derive(Debug)]
 struct Rect {
     h: i32,
     w: i32,
+}
+
+// Below are the methods of the above struct
+impl Rect {
+    fn area(&self) -> i32 {
+        self.h * self.w
+    }
+
+    fn perimeter(&self) -> i32 {
+        2 * (self.h + self.w)
+    }
+}
+
+struct Person {
+    name: String,
+    age: i32,
+    gender: String,
+    // is_alive: bool,
+}
+
+trait PrintPerson {
+    fn print(&self);
+}
+
+impl PrintPerson for Person {
+    fn print(&self) {
+        println!(
+            "{} is {} years old. His gender is {}",
+            self.name, self.age, self.gender
+        )
+    }
 }
 
 fn main() {
@@ -13,5 +46,16 @@ fn main() {
     } = rect;
 
     println!("H {height} x W {width}");
-    println!("{:?}", rect)
+    // println!("{:?}", rect);
+    println!("Area of rectangle: {}", rect.area());
+    println!("Perimeter of rectangle: {}", rect.perimeter());
+
+    let person: Person = Person {
+        name: String::from("John Doe"),
+        age: 25,
+        gender: String::from("Male"),
+        // is_alive: true,
+    };
+
+    person.print();
 }
